7fd645104e5980ba656498fcac4d77a4c2218a19
Lingfeng Fu <2871618714@qq.com>
2025-03-28 16:54:47 +0800

    simple separation constraint

--- Assets/_Gems1/C3S7_Flocking/Scripts/BoidShip.cs
+++ Assets/_Gems1/C3S7_Flocking/Scripts/BoidShip.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace _Gems1.C3S7_Flocking.Scripts
@@ -11,17 +12,22 @@ namespace _Gems1.C3S7_Flocking.Scripts
 		[Serializable]
 		internal class Config
 		{
-			public Transform Target;
+			[Header("Global")] public Transform Target;
 			public float MaxSpeed = 3.73f;
 			public float MaxAcceleration = 1;
-			public float WeightOfSeparation = 1;
+
+			[Header("Accumulation weights")] public float WeightOfSeparation = 1;
 			public float WeightOfAlignment = 1;
 			public float WeightOfCohesion = 1;
-			public float PerceptionRange = 2;
-			public float CohesionRange = 1;
+
+			[Header("Perception")] public float PerceptionRange = 2;
+			[Header("Separation")] public float SeparationRange = 0.5f;
+			[Header("Cohesion")] public float CohesionRange = 1.5f;
 		}
 
 		private Config _Config;
+
+		[SerializeField, ReadOnly] private float _TargetDistance;
 
 		internal void Init(Config config)
 		{
@@ -34,6 +40,8 @@ namespace _Gems1.C3S7_Flocking.Scripts
 			TickAccelerationInput(movementInput);
 
 			base.Update();
+
+			_TickDebugInfo();
 		}
 
 		private Vector3 _SolveConstraints()
@@ -52,7 +60,18 @@ namespace _Gems1.C3S7_Flocking.Scripts
 
 		private Vector3 _SolveSeparationConstraint()
 		{
-			return Vector3.zero;
+			var change = Vector3.zero;
+
+			var translation = _Config.Target.position - transform.position;
+			var distance = translation.magnitude;
+			if (distance < _Config.SeparationRange)
+			{
+				var separationAcc = _Config.SeparationRange / Mathf.Max(distance, 0.01f);
+				separationAcc = Mathf.Min(separationAcc, 1.0f);
+				change -= translation.normalized * separationAcc;
+			}
+
+			return change;
 		}
 
 		private Vector3 _SolveAlignmentConstraint()
@@ -71,6 +90,11 @@ namespace _Gems1.C3S7_Flocking.Scripts
 
 			var direction = translation / distance;
 			return direction;
+		}
+
+		private void _TickDebugInfo()
+		{
+			_TargetDistance = (_Config.Target.position - transform.position).magnitude;
 		}
 	}
 }
